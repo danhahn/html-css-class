05c2b262a4dfce4edf0fc63dc2e8ec6449e07691
Dan Hahn <daniel.hahn@michaelkors.com>
2017-12-06 13:58:07 -0500

    Remove Old lesson files

--- contents/lessons/lesson-6/lesson/Exercises/sandbox.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-	$("#hello-world-button").click(function() {
-		var paragraph_fragment = $('<p>Hello World!</p>');
-
-		$('#paragraph-container').append(paragraph_fragment);
-	});
-});
